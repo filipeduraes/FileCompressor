462d4c9b378a59570f96eab3f59f64c1cdc2f76c
Ana Júlia Oliveira <anajulia.oliveyra@gmail.com>
2024-11-19 22:07:20 -0300

    Update FileHandler.cpp
    LoadTextFile and LoadBinaryFile Updated

--- FileCompressor/FileHandler.cpp
+++ FileCompressor/FileHandler.cpp
@@ -17,17 +17,62 @@ void FileHandler::SaveBinaryFile(const Compressor::CompressorOutput& data)
 
 std::string FileHandler::LoadTextFile()
 {
-    return "";
+    std::ifstream stream (path);
+    std::string line;
+    std::string result;
+
+    while (std::getline(stream,line))
+        {
+            result+= line;    
+        }
+
+        stream.close();
+
+    return result;
 }
 
 Compressor::CompressorOutput FileHandler::LoadBinaryFile()
 {
+    std::ifstream stream(path);
+    std::string line;
+    std::vector<std::string> lines;
+
+    while (std::getline(stream,line ))
+    {
+        lines.push_back(line);
+    }
+
     std::map<std::string, std::string> compressionTable;
-    uint64_t initialBitSize = 0;
-    std::vector<uint8_t> compressedBytes;
     
-    return {compressionTable, initialBitSize, compressedBytes};
-}
+    for (int i = 0; i < lines.size() - 2; i++)
+    {
+        line = lines[i];
+        uint64_t separatorIndex = line.find_last_of(':');
+    
+        std::string key = line.substr(0,separatorIndex);
+        std::string valor = line.substr(separatorIndex + 1, line.size() - separatorIndex);
+    
+        compressionTable[key] = valor;
+
+    }
+        uint64_t initialBitSize = 0;
+        std::stringstream sizeStream (lines[lines.size()-2]);
+        sizeStream.read(reinterpret_cast<char*>(&initialBitSize), sizeof(initialBitSize));
+    
+        std::vector<uint8_t> compressedBytes;
+        std::stringstream compressedStream (lines[lines.size()-1]);
+
+        while(!compressedStream.eof())
+        {
+            uint8_t byte;
+            compressedStream.read(reinterpret_cast<char*>(&byte), sizeof(byte));
+            compressedBytes.push_back(byte);
+        }
+        
+        stream.close();
+
+        return {compressionTable, initialBitSize, compressedBytes};
+    }
 
 std::string FileHandler::GetOutputPath(const bool isCompressed) const
 {
